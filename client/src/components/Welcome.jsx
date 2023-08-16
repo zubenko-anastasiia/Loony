b5946633c6bde39f9b3c20771b3a5000827610b1
Anastasiia Zubenko <nastya.zubenko.05@gmail.com>
2023-08-16 14:27:36 +0300

    navbar+welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -8,7 +8,7 @@ const Welcome =()=>{
     return(
         <div className="flex w-full justify-center items-center">
             <div className=" flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                <div className="flex flex-1 text-white justify-start flex-col md:mr-10">
                     <h1>
                         Send Crypto <br/> across the world
                     </h1>
