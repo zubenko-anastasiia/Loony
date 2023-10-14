c604336dc287ba19ffdd665655fc07004207772f
Anastasiia Zubenko <nastya.zubenko.05@gmail.com>
2023-10-14 19:02:56 +0300

    fixed warning add material-tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,7 +9,14 @@
 //   },
 //   plugins: [],
 // }
-
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   mode: "jit",
