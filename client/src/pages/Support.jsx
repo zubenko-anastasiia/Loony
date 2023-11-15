5a0a703f8f368913c8e877c6350952dde21c9241
Anastasiia Zubenko <nastya.zubenko.05@gmail.com>
2023-11-15 21:50:03 +0200

    support end transfer pages --complete

--- client/src/pages/Support.jsx
+++ client/src/pages/Support.jsx
@@ -1,8 +1,139 @@
+import React, { useState } from 'react';
+const Input = ({placeholder,name,type,value, handleChange}) =>(
+    <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e)=> handleChange(e, name) }
+    className=" w-full h-16 rounded-sm p-2 outline-none bg-transparent text-white placeholder-gray-200 border-none text-sm white-glassmorphism "
+    />
+  );
+
 const Support =()=>{
+    const [show, setShow] = useState(false);
     return(
-        <>
-            <h1>Support</h1>
-        </>
+    <div className="container my-24 mx-auto md:px-6">
+        <section className="mb-32">
+            <div className="flex flex-wrap">
+
+            <div className="grid-cols-1 sm:grid md:grid-cols-3 ">
+
+                <div className="mx-3 mt-6 flex flex-col rounded-lg bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 sm:shrink-0 sm:grow sm:basis-0 ">
+                    <div className="p-6">
+                        <h5
+                            className="mb-2 text-center text-xl leading-tight text-white ">
+                            Card title
+                        </h5>
+                        <p className="mb-4 text-center text-neutral-800">
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                    </div>
+                </div>
+                <div className="mx-3 mt-6 flex flex-col rounded-lg bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 sm:shrink-0 sm:grow sm:basis-0 ">
+                    <div className="p-6">
+                        <h5
+                            className="mb-2 text-center text-xl leading-tight text-white ">
+                            Card title
+                        </h5>
+                        <p className="mb-4 text-center text-neutral-800">
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                    </div>
+                </div>
+                <div className="mx-3 mt-6 flex flex-col rounded-lg bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 sm:shrink-0 sm:grow sm:basis-0 ">
+                    <div className="p-6">
+                        <h5
+                            className="mb-2 text-center text-xl leading-tight text-white ">
+                            Card title
+                        </h5>
+                        <p className="mb-4 text-center text-neutral-800">
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                    </div>
+                </div>
+                <div className="mx-3 mt-6 flex flex-col rounded-lg bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 sm:shrink-0 sm:grow sm:basis-0 ">
+                    <div className="p-6">
+                        <h5
+                            className="mb-2 text-center text-xl leading-tight text-white ">
+                            Card title
+                        </h5>
+                        <p className="mb-4 text-center text-neutral-800">
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                    </div>
+                </div>
+                <div className="mx-3 mt-6 flex flex-col rounded-lg bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 sm:shrink-0 sm:grow sm:basis-0 ">
+                    <div className="p-6">
+                        <h5
+                            className="mb-2 text-center text-xl leading-tight text-white ">
+                            Card title
+                        </h5>
+                        <p className="mb-4 text-center text-neutral-800">
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                    </div>
+                </div>
+                <div className="mx-3 mt-6 flex flex-col rounded-lg bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 sm:shrink-0 sm:grow sm:basis-0 ">
+                    <div className="p-6">
+                        <h5
+                            className="mb-2 text-center text-xl leading-tight text-white ">
+                            Card title
+                        </h5>
+                        <p className="mb-4 text-center text-neutral-800">
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+    
+        
+      {/* </div>
+      <div className="sm:w-96 w-full flex flex-col justify-center items-center blue-glassmorphism">
+        <p className="mt-5 mb-8 font-bold">
+          Contact us
+        </p>
+        <form>
+          <div className="relative mb-6" data-te-input-wrapper-init>
+          <input type="text" className="peer block min-h-[auto] w-full rounded border-1 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="exampleInput90" placeholder="Name" />
+            <label className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary" for="exampleInput90">Name
+            </label>
+          </div> 
+          <div className="relative mb-6" data-te-input-wrapper-init>
+            <input type="email" className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="exampleInput91" placeholder="Email address" />
+            <label className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary" for="exampleInput91">Email address
+            </label>
+          </div>
+          <div className="relative mb-6" data-te-input-wrapper-init>
+            <textarea className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0" id="exampleFormControlTextarea1" rows="3" placeholder="Your message"></textarea>
+            <label for="exampleFormControlTextarea1" className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">Message</label>
+          </div>
+          
+          <button type="button" data-te-ripple-init data-te-ripple-color="light"
+            className="mb-6 inline-block w-full rounded-full bg-[#8899f2] px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-[#3725bd] hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+            Send
+          </button>
+
+        </form>
+      </div>
+     */}
+     </div>
+  </section>
+  
+</div>
     );
   }
   
--- client/src/pages/Transfer.jsx
+++ client/src/pages/Transfer.jsx
@@ -1,26 +1,56 @@
-const Transfer =()=>{
-  return(
-      <>
-          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input className="placeholder-white" placeholder="Отримувач" name="addressTo" type="text" handleChange={()=>{}} placeholder-white/>
-            <Input className="placeholder-white" placeholder="Кількість (ETH)" name="amount" type="number" handleChange={()=>{}}/>
-            <Input className="placeholder-white" placeholder="Повідомлення" name="message" type="text" handleChange={()=>{}}/>
-            <div className="h-[1px] w-full bg-white my-2"/>
-                            
-                            {false ? (
-                                <Loader/>
-                            ) : (
-                                <button
-                                type="button"
-                                onClick={handleSubmit}
-                                className="text-white w-full mt-2 border-[1px] p-2 border-[#be8defd5] rounded-full cursor-pointer  hover:bg-[#be8defd5] header-glassmorphism">
-                                    Надіслати
-                                </button>
-                            )}
-             </div>
-            
-      </>
-  );
-}
+import { useTranslation } from 'react-i18next';
 
-export default Transfer;
+const Input = ({placeholder,name,type,value, handleChange}) =>(
+    <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e)=> handleChange(e, name) }
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism "
+    />
+  );
+  const locales = {
+    en: { title: 'English' },
+    ua: { title: 'Українська' },
+  };
+  const Transfer =()=>{
+    const connectWallet = ()=>{
+  
+    }
+  
+    const handleSubmit = ()=>{
+  
+    }
+    const { t, i18n } = useTranslation();
+    return(
+        <div className="flex w-full justify-center items-center">
+        <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+          
+            <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+              
+            <div className="p-5 sm:w-96 w-full mt-10 flex flex-col justify-start items-center blue-glassmorphism">
+              <div className=" justify-center text-center font-bold text-2xl text-gray-700"> {t('system.transfer')}</div>
+              <Input className="placeholder-white mt-6" placeholder="Отримувач" name="addressTo" type="text" handleChange={()=>{}} placeholder-white/>
+              <Input className="placeholder-white" placeholder="Кількість (ETH)" name="amount" type="number" handleChange={()=>{}}/>
+              <Input className="placeholder-white" placeholder="Повідомлення" name="message" type="text" handleChange={()=>{}}/>
+              <div className="h-[1px] w-full bg-white my-2"/>
+                              
+                              {false ? (
+                                  <Loader/>
+                              ) : (
+                                  <button
+                                  type="button"
+                                  onClick={handleSubmit}
+                                  className="text-white w-full mt-2 border-[1px] p-2 border-[#be8defd5] rounded-full cursor-pointer  hover:bg-[#be8defd5] header-glassmorphism">
+                                      Надіслати
+                                  </button>
+                              )}
+               </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Transfer;
